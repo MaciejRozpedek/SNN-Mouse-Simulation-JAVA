45901da3ae2180993b97fc378cbc70d7d46aaad2
Maciej Rozpedek <maciej.rozpedek@gmail.com>
2025-12-11 17:36:00 +0100

    Initial Spring Boot project structure

---
+++ src/main/java/com/macroz/snnmousesimulation/SnnMouseSimulationApplication.java
@@ -0,0 +1,13 @@
+package com.macroz.snnmousesimulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnnMouseSimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnnMouseSimulationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/macroz/snnmousesimulation/SnnMouseSimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macroz.snnmousesimulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnnMouseSimulationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
